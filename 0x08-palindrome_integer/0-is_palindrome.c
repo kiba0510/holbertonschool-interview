aa5da8ce91ae0f83e4127dbf70a3028bb8d5e167
kiba0510 <kiba0510@gmail.com>
2021-09-13 09:37:45 -0500

    Palindrome unsigned integer

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - check if interger is a palindrome
+ * @n: integer
+ * Return: 1 if True else 0
+ */
+int is_palindrome(unsigned long n)
+{
+    unsigned long rev = 0;
+    unsigned long rem;
+    unsigned long ori_num;
+
+    ori_num = n;
+
+    while (n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+
+    if (ori_num == rev)
+        return (1);
+    else
+        return (0);
+}
